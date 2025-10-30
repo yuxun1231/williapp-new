8247f56ad5bb6849c1f88cb9b2cded7368548e0e
williamyooda <162965976+williamyooda@users.noreply.github.com>
2025-10-30 13:55:47 +0800

    Add project files.

---
+++ williapp/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace williapp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Color strokeColor = Colors.Black;
+        Color fillColor = Colors.Transparent;
+        int strokeThickness = 1;
+        string actionType = "Draw";
+        string shapeType = "Line";
+        Point start, end;
+        public MainWindow()
+        {
+            InitializeComponent();
+            outline.SelectedColor = strokeColor;
+            inner.SelectedColor = fillColor;
+            
+        }
+
+    }
+}
